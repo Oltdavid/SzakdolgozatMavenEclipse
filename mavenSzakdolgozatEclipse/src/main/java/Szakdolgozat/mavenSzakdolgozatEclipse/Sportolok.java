d8b3a8acc9e4627cabc45de794c55a2b212aa0f0
Oltvanyi David <oltdavid@gmail.com>
2018-11-10 10:38:49 +0100

    Maven Eclipse-ben működik!

---
+++ mavenSzakdolgozatEclipse/src/main/java/Szakdolgozat/mavenSzakdolgozatEclipse/Sportolok.java
@@ -0,0 +1,25 @@
+package Szakdolgozat.mavenSzakdolgozatEclipse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Sportolok extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Sportolók");
+        stage.setWidth(1390);
+        stage.setHeight(650);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
